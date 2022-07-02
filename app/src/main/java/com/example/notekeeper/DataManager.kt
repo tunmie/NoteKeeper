088ddb17519a623655d27029006710c18e44af02
tunmie <victoroluwasomidotun@gmail.com>
2022-07-02 09:31:31 +0100

    ui for noteinfo done

--- app/src/main/java/com/example/notekeeper/DataManager.kt
+++ app/src/main/java/com/example/notekeeper/DataManager.kt
@@ -1,4 +1,21 @@
 package com.example.notekeeper
 
 class DataManager {
+	val courses = HashMap<String, CourseInfo>()
+
+	val notes = ArrayList<NoteInfo>()
+
+	private fun initializeCourses() {
+		var course = CourseInfo("android_with_intents", "Android Programming with Intents")
+		courses.set(course.courseId, course)
+
+		course = CourseInfo("android_async", "Android Async Programming and Services")
+		courses.set(course.courseId, course)
+
+		course = CourseInfo(courseId = "java_lang", title = "Java Fundamentals: The Java Language")
+		courses.set(course.courseId, course)
+
+		course = CourseInfo(title = "Java Fundamentals: The Core Platform", courseId = "java_core")
+		courses.set(course.courseId, course)
+	}
 }
